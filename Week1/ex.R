6e1789dbdd19e5a2be71da42fb61309c0f456467
seominji <55734436+jaaaamj0711@users.noreply.github.com>
2021-08-28 16:25:43 +0900

    Update ex.R

--- Week1/ex.R
+++ Week1/ex.R
@@ -38,4 +38,53 @@ food_consumption %>%
 # Calculate the quartiles of co2_emission
 quantile(food_consumption$co2_emission)
 
+# Calculate variance and sd of co2_emission for each food_category
+food_consumption %>% 
+  group_by(food_category) %>% 
+  summarize(var_co2 = var(co2_emission),
+     sd_co2 = sd(co2_emission))
+
+# Plot food_consumption with co2_emission on x-axis
+ggplot(food_consumption, aes(x=co2_emission)) +
+  # Create a histogram
+  geom_histogram() +
+  # Create a separate sub-graph for each food_category
+  facet_wrap(~food_category)
+
+# Calculate total co2_emission per country: emissions_by_country
+emissions_by_country <- food_consumption %>%
+  group_by(country) %>%
+  summarize(total_emission = sum(co2_emission))
+
+emissions_by_country
+
+# Count the deals for each product
+amir_deals %>%
+  count(product)
+
+# Set random seed to 31
+set.seed(31)
+
+# Sample 5 deals without replacement
+amir_deals %>%
+  sample_n(5)
+
+# Create a histogram of group_size
+restaurant_groups %>%
+ggplot(aes(x=group_size)) +
+  geom_histogram(bins=5)
+
+# Expected number won with 30% win rate
+won_30pct <- 3 * 0.3
+won_30pct
+
+# Expected number won with 25% win rate
+won_25pct <- 3 * 0.25
+won_25pct
+
+# Expected number won with 35% win rate
+won_35pct <- 3 * 0.35
+won_35pct
+
+
 
